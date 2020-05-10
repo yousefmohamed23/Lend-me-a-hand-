a3dee1ccb3f8508dab5c51748e437207bb73a8f2
yousefmohamed23 <36644002+yousefmohamed23@users.noreply.github.com>
2020-05-10 20:55:44 +0200

    Update contest.py

--- contest.py
+++ contest.py
@@ -24,4 +24,23 @@ elif k == ord('r'):  # press 'r' to reset the background
     bgModel = None
     isBgCaptured = 0
 
-
+#starting camera 
+    camera = cv2.VideoCapture(0)
+    camera.set(10, 250)
+    cv2.namedWindow('trackbar')
+    cv2.createTrackbar('trh1', 'trackbar', threshold, 100, printThreshold)
+	
+    while camera.isOpened():
+	
+        #opens video for user
+        ret, frame = camera.read()
+		#apply thershold 
+        threshold = cv2.getTrackbarPos('trh1', 'trackbar')
+		# smoothing filter
+        frame = cv2.bilateralFilter(frame, 5, 50, 100)
+		# flip the frame horizontally			
+        frame = cv2.flip(frame, 1)  
+        
+		cv2.rectangle(frame, (int(capturingBGregionX * frame.shape[1]), 0), (frame.shape[1], int(capturingBGregionY * frame.shape[0])), (255, 0, 0), 2)
+        
+		cv2.imshow('background capture', frame)
