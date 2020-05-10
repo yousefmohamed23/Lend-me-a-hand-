94b2f441ecba151f9a673dd5733575f082878899
Mostafa Mohamed Abdelmegeed <36315590+MostafaMohamed11@users.noreply.github.com>
2020-05-11 01:30:04 +0200

    Calculate Fingers

--- contest.py
+++ contest.py
@@ -8,6 +8,30 @@ def removeBG(image):
     fgmask = cv2.erode(fgmask, kernel, iterations=1)
     res = cv2.bitwise_and(image, image, mask=fgmask)
     return res
+    # function to draw contours of fingers and count them
+def calculateFingers(res, drawing):
+   #  convexity defect
+    hull = cv2.convexHull(res, returnPoints=False)
+    if len(hull) > 3:
+        defects = cv2.convexityDefects(res, hull)
+        if type(defects) != type(None):  # avoid crashing.   (BUG not found)
+		
+            count = 0
+	
+        for i in range(defects.shape[0]):  # calculate the angle
+            s, e, f, d = defects[i][0]
+            start = tuple(res[s][0])
+            end = tuple(res[e][0])
+            far = tuple(res[f][0])
+	    a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
+            b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
+            c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
+            angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))  # cosine theorem
+            if angle <= math.pi / 2:  # angle less than 90 degree, treat as fingers
+                count += 1
+		cv2.circle(drawing, far, 8, [211, 84, 0], -1)
+        return True, count
+    return False, 0
 
 #starting camera 
 camera = cv2.VideoCapture(0)
@@ -43,11 +67,14 @@ cv2.createTrackbar('trh1', 'trackbar', threshold, 100, printThreshold)
 		y, x = np.argwhere(nz > 128)[0]
 		highest_point_image = cv2.circle(thresh, (X, Y), 5, white, -1)
 		cv2.imshow('ori', highest_point_image)
+		
 	thresh1 = copy.deepcopy(thresh)
+	
         # Getting contours of hand
         _, contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         length = len(contours)
         maxArea = -1
+	
         if length > 0:
 	    for i in range(length):  # find the biggest contour (according to area)
                 temp = contours[i]
@@ -66,7 +93,9 @@ cv2.createTrackbar('trh1', 'trackbar', threshold, 100, printThreshold)
             if triggerSwitch is True:
                 if isFinishCal is True and cnt <= 2:
                     print(cnt) #number of fingers that is present in frame
+		
         cv2.imshow('output', drawing)
+	
 	 # paint interface
     	paintWindow = np.zeros((471, 636, 3)) + 255
     	cv2.namedWindow('Paint', cv2.WINDOW_AUTOSIZE)
